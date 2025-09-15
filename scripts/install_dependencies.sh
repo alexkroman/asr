6cc47ff7227f2b5dad2fbbbeff4e684ee66c9a5a
alexkroman <alex@assemblyai.com>
2025-09-15 11:42:17 -0700

    update deploy scripts

--- scripts/install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt update
-echo "⬆️  Upgrading all system packages..."
-apt upgrade -y
-apt install -y ffmpeg libsndfile1 libsndfile1-dev
-pip install --upgrade pip
-cd .. && pip install -e ".[cuda,optimized]" && cd scripts
-pip install --upgrade --force-reinstall torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
